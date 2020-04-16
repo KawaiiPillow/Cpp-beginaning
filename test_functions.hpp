c8c9d6439f4afb35145f7072c91ebf1c6cb47193
William White <63733033+KawaiiPillow@users.noreply.github.com>
2020-04-16 17:44:08 +0200

    Create test_functions.hpp

---
+++ test_functions.hpp
@@ -0,0 +1,4 @@
+double test_thing(double area, double type_price, double total_price);
+double area_choice_func_1(double area, double len, double width);
+double area_choice_func_2(double area, double len, double width, int num_tiles);
+int wastage_func(int area);
